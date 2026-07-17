ecdc608083426e7bfc6ec194f3b1cedb2163437a
SideCat404 <sidekitten@mail.ru>
2026-07-17 22:28:59 +0700

    Papka2 рабочий вариант

---
+++ accountsMenager.cs
@@ -0,0 +1,28 @@
+using System.Text;
+using System.Text.Json;
+
+public class AccountProcessor
+{
+    public static string filePath = "wwwroot/accounts.json";
+    
+    public List<Account> LoadAccounts()
+    {
+        try
+        {
+            string jsonStr = File.ReadAllText(filePath, Encoding.UTF8);
+            var accounts = JsonSerializer.Deserialize<List<Account>>(jsonStr);
+            return accounts ?? new List<Account>();
+        }
+        catch
+        {
+            return new List<Account>();
+        }
+    }
+}
+
+public class Account
+{
+    public string name { get; set; } = string.Empty;
+    public string login { get; set; } = string.Empty;
+    public string password { get; set; } = string.Empty;
+}
