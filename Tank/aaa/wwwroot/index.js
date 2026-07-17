b10aec7c94bd16dc7733e933b8a663e87c862a27
Tank1411 <zayts.tank@gmail.com>
2026-07-17 14:54:42 +0700

    Сделала появление кнопочек всяких и тд (щас будем делать убирание)

---
+++ Tank/aaa/wwwroot/index.js
@@ -0,0 +1,84 @@
+console.log(
+    "эуэапапээ");
+
+function sayHello() {
+    alert("Саламчик");
+}
+
+
+const fdfd =
+    document.getElementById(
+        "fdfd");
+
+console.log(fdfd);
+
+const nameInput = 
+        document.getElementById(
+            "nameInput");
+
+function dsds() {
+    const name = 
+        nameInput.value; //  текущее значение поля.
+
+    console.log(name);
+}
+
+const messages = 
+    document.getElementById(
+        "messages");
+
+const message = 
+        document.createElement("div");
+
+message.textContent = 
+        "тексь";
+
+message.className = 
+        "message";
+    
+messages.appendChild(message);
+
+
+
+function hideMmm() {
+    const hidedtext = 
+        document.getElementById(
+            "mmm");
+
+    console.log(hidedtext);
+
+
+    hidedtext.classList.remove('hide');
+    // hidedtext.classList.add('hide');
+}
+
+const title =
+    document.getElementById(
+        "title");
+
+function startGame() {
+    title.textContent =
+        "Игра началась";
+
+    const hidedtext = 
+        document.getElementById(
+            "afterStart");
+
+    console.log(hidedtext);
+
+
+    hidedtext.classList.remove('hide');
+    // hidedtext.classList.add('hide');
+}
+
+function hideMmm() {
+    const hidedtext = 
+        document.getElementById(
+            "mmm");
+
+    console.log(hidedtext);
+
+
+    hidedtext.classList.remove('hide');
+    // hidedtext.classList.add('hide');
+}
