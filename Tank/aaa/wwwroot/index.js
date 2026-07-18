a80f8316d2b078025c3f6707ab766fe49f4ab7d8
Tank1411 <zayts.tank@gmail.com>
2026-07-18 12:00:54 +0700

    Интерфейс гтов (без оформы есче)

--- Tank/aaa/wwwroot/index.js
+++ Tank/aaa/wwwroot/index.js
@@ -1,42 +1,3 @@
-console.log(
-    "эуэапапээ");
-
-function sayHello() {
-    alert("Саламчик");
-}
-
-
-const fdfd =
-    document.getElementById(
-        "fdfd");
-
-console.log(fdfd);
-
-const nameInput = 
-        document.getElementById(
-            "nameInput");
-
-function dsds() {
-    const name = 
-        nameInput.value; //  текущее значение поля.
-
-    console.log(name);
-}
-
-const messages = 
-    document.getElementById(
-        "messages");
-
-const message = 
-        document.createElement("div");
-
-message.textContent = 
-        "тексь";
-
-message.className = 
-        "message";
-    
-messages.appendChild(message);
 
 
 
@@ -60,14 +21,6 @@ function startGame() {
     title.textContent =
         "Игра началась";
 
-    const hidedtext = 
-        document.getElementById(
-            "afterStart");
-
-    console.log(hidedtext);
-
-
-    hidedtext.classList.remove('hide');
     // hidedtext.classList.add('hide');
 }
 
@@ -81,4 +34,50 @@ function openMmm() {
 
     hidedtext.classList.remove('hide');
     // hidedtext.classList.add('hide');
+}
+
+function hideNnn() {
+    const openedtext = 
+        document.getElementById(
+            "nnn");  //Кавычки ставятся если это константа, без ковычек - если параметр
+ 
+    console.log(openedtext);
+
+    openedtext.classList.add('hide');
+}
+
+function openMmmhideNnn() {
+    openMmm();
+    hideNnn();
+}
+
+
+function hideXStartGame() {
+    hideX("hide-when-press-start", false)
+    startGame();
+    hideX("open-when-press-start", true)
+}
+
+
+function hideX(idhtmlElement, visible) {
+    const openedtext = 
+        document.getElementById(
+            idhtmlElement); 
+
+    console.log(openedtext);
+
+if (visible == true) {
+    openedtext.classList.remove('hide')
+}
+else {
+    openedtext.classList.add('hide')
+}
+}
+
+function keydown(event)
+{
+    console.log(event.key);
+    if (event.key == 'Enter')
+    console.log(
+        "Nnnn");
 }
